436c22aaa6d604f0e4c32c07d5f891b6f135385b
Bo Liang <bo.liang@pearson.com>
2016-12-19 16:14:35 -0500

    Cleanup

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,4 +1,4 @@
-rails_env = new_resource.environment["RAILS_ENV"]  
+rails_env = new_resource.environment["RAILS_ENV"]
 current_release = release_path
 
 if node[:opsworks][:instance][:layers].first == "rails-app"
