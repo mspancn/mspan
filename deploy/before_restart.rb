70bb64b9ccccca6af2fa963972365dbccd7e73cc
Bo Liang <bo.liang@pearson.com>
2017-01-06 18:38:45 -0500

    Move ses yml creation to before_restart

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,4 +1,16 @@
+require 'yaml'
+
 if node[:opsworks][:instance][:layers].first == "rails-app"
+  # NOTE: create aws ses yml
+  ::File.open("#{release_path}/config/aws_ses.yml", "w") do |f|
+    f.write({
+      node[:deploy][:mspan_rails][:rails_env] => {
+        "access_key_id"     => node[:deploy][:mspan_rails][:aws_ses][:access_key_id],
+        "secret_access_key" => node[:deploy][:mspan_rails][:aws_ses][:secret_access_key]
+      }
+    }.to_yaml)
+  end
+
   # NOTE: allow origin * for fonts when we deploy through Apache
   if ::File.directory?("#{node[:apache][:dir]}/sites-available/mspan_rails.conf.d")
     ::File.open("#{node[:apache][:dir]}/sites-available/mspan_rails.conf.d/local-cross-origin", "w") do |f|
