cbf2e533869251b25b2e5088f275d1d9383b9c9c
Bo Liang <bo.liang@pearson.com>
2017-01-06 17:44:16 -0500

    Move aws_ses.yml writing to before_symlink

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,5 +1,3 @@
-require 'yaml'
-
 if node[:opsworks][:instance][:layers].first == "rails-app"
   # NOTE: allow origin * for fonts when we deploy through Apache
   if ::File.directory?("#{node[:apache][:dir]}/sites-available/mspan_rails.conf.d")
@@ -8,15 +6,5 @@ if node[:opsworks][:instance][:layers].first == "rails-app"
       f.write("Header set Access-Control-Allow-Origin \"*\"\n")
       f.write("</FilesMatch>\n")
     end
-  end
-
-  # NOTE: create aws ses yml
-  ::File.open("#{release_path}/config/aws_ses.yml", "w") do |f|
-    f.write({
-      node[:deploy][:mspan_rails][:rails_env] => {
-        "access_key_id"     => node[:deploy][:mspan_rails][:aws_ses][:access_key_id],
-        "secret_access_key" => node[:deploy][:mspan_rails][:aws_ses][:secret_access_key]
-      }
-    }.to_yaml)
   end
 end
--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,10 +1,20 @@
-rails_env = new_resource.environment["RAILS_ENV"]
-current_release = release_path
+require 'yaml'
 
 if node[:opsworks][:instance][:layers].first == "rails-app"
+
+  # NOTE: create aws ses yml
+  ::File.open("#{release_path}/config/aws_ses.yml", "w") do |f|
+    f.write({
+      node[:deploy][:mspan_rails][:rails_env] => {
+        "access_key_id"     => node[:deploy][:mspan_rails][:aws_ses][:access_key_id],
+        "secret_access_key" => node[:deploy][:mspan_rails][:aws_ses][:secret_access_key]
+      }
+    }.to_yaml)
+  end
+
   execute "rake assets:precompile" do
-    cwd current_release
+    cwd release_path
     command "bundle exec rake assets:precompile"
-    environment "RAILS_ENV" => rails_env
+    environment "RAILS_ENV" => node[:deploy][:mspan_rails][:rails_env]
   end
 end
