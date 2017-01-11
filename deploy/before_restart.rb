b7dcafe87a00fa51c6145c1b9023955f1b537f56
Bo Liang <bo.liang@pearson.com>
2017-01-11 15:28:07 -0500

    Add redirect rules

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -6,5 +6,24 @@ if node[:opsworks][:instance][:layers].first == "rails-app"
       f.write("Header set Access-Control-Allow-Origin \"*\"\n")
       f.write("</FilesMatch>\n")
     end
+
+    ::File.open("#{node[:apache][:dir]}/sites-available/mspan_rails.conf.d/rewrite", "w") do |f|
+      if node[:deploy][:mspan_rails][:rails_env] == "ppe"
+        # Redirect *.mspan.cn to https://mspan.cn
+        f.write("RewriteCond %{HTTP_HOST} ^(.)+\\.mspan\\.cn$\n")
+        f.write("RewriteRule ^(.*)$ http://mspan.cn$1 [R,L]\n")
+
+        # Redirect mspan.us to https://mspan.cn/teacherhome
+        f.write("RewriteCond %{HTTP_HOST} ^mspan\\.us$\n")
+        f.write("RewriteRule ^(.*)$ http://mspan.cn/teacherhome [R,L]\n")
+
+        # Redirect *.elb.amazonaws.com to https://mspan.cn
+        f.write("RewriteCond %{HTTP_HOST} ^(.)+\\.elb\\.amazonaws\\.com$\n")
+        f.write("RewriteRule ^(.*)$ http://mspan.cn$1 [R,L]\n")
+      end
+      # Force https
+      # f.write("RewriteCond %{HTTP:X-Forwarded-Proto} !https\n")
+      # f.write("RewriteRule !/status https://%{SERVER_NAME}%{REQUEST_URI} [R,L]\n")
+    end
   end
 end
