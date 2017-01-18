d96cdac701bd34a29c3576a863ed489709982af2
Bo Liang <bo.liang@pearson.com>
2017-01-18 15:18:33 -0500

    Show video requirement page for admins

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   include Accessible
-  before_action :check_user, except: [:status]
+  before_action :check_user, except: [:status, :video_requirement]
 
   caches_action :about, :contact, :guidance, :index, :teacherfaq, :teacherhome, :terms, :video_requirement, expires_in: 1.day
 
