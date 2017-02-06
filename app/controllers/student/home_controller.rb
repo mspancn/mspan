fd557a86f321ead789f1017dd01c2047deff1802
Xin Chen <panyachen157@gmail.com>
2017-02-05 20:44:16 -0500

    minor fix

--- app/controllers/student/home_controller.rb
+++ app/controllers/student/home_controller.rb
@@ -5,11 +5,5 @@ class Student::HomeController < StudentController
     @next_scheduled_appointment = current_student.next_scheduled_appointment
     @uncompleted_appointments = current_student.uncompleted_appointments
   end
-
-  def contact
-  end
-
-  def follow
-  end
-
+  
 end
